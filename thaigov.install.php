9584b845be66321e91fb1004588d9f9d968ae143
Wasan Wangrach <maxasus123@gmail.com>
2017-04-05 15:53:27 +0700

    Edit info file.

--- thaigov.install.php
+++ thaigov.install.php
@@ -1,17 +1,20 @@
 <?php
-
 /**
  * @file
- * Install, update and uninstall functions for the aGov profile.
+ * Install, update and uninstall functions for the profilename install profile.
  */
 
 /**
  * Implements hook_install().
+ *
+ * Perform actions to set up the site for this profile.
+ *
+ * @see system_install()
  */
-function thaigov_install() {
-  // The system.site config entity must be configured during install.
-  \Drupal::configFactory()->getEditable('system.site')
-    ->set('mail', 'admin@drupalthai.org')
-    ->set('name', 'ThaiGov')
-    ->save();
+function profilename_install() {
+  // First, do everything in standard profile.
+  include_once DRUPAL_ROOT . '/core/profiles/standard/standard.install';
+  standard_install();
+  
+  // Can add code in here to make nodes, terms, etc.
 }
